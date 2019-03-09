bae6f01a3b0f7fea3bd1df80821010426b9e0ba4
Rafael S. de Souza <rafael.2706@gmail.com>
2019-03-09 12:00:26 -0500

    :+1:

--- DALEX_RF.R
+++ DALEX_RF.R
@@ -95,21 +95,6 @@ classif_glm <- glm(PREGNANT_NUMERIC~LIGATION_GROUP+AGE+TL_rand + TLD_rand +
                    family=binomial(link = "logit"))
 
 
-#classif_svm <- ksvm(PREGNANT_NUMERIC~LIGATION_GROUP+AGE+TL_rand + TLD_rand + TLP_rand  + 
-#                    ANAS_rand + Fibr_rand,data = Train,
- #                   kernel="splinedot",type="C-svc",prob.model = TRUE)
-
-classif_ann <- neuralnet(PREGNANT_NUMERIC~LIGATION_GROUP+AGE+TL_rand + TLD_rand + TLP_rand  + 
-                      ANAS_rand + Fibr_rand,data = Train,hidden = c(3,3),linear.output = F)
-
-
-
-classif_gbm <- gbm(PREGNANT_NUMERIC~LIGATION_GROUP+AGE+TL_rand + TLD_rand + TLP_rand  + 
-               ANAS_rand + Fibr_rand,data = Train,
-               distribution = "bernoulli", n.trees = 500, shrinkage = 0.1)
-
-
-
 classif_rf <-  randomForest(PREGNANT_NUMERIC~LIGATION_GROUP+AGE+TL_rand + 
                             TLD_rand + TLP_rand  + ANAS_rand + Fibr_rand, 
                             data = Train, ntree=2500)
@@ -127,11 +112,6 @@ explain_glm  <- explain(classif_glm, label = "GLM",
                                         data = Test[,-1], y = yTest,
                                  predict_function = p_glm)
 
-explain_ann  <- explain(classif_ann, label = "ANN", 
-                                        data = Test[,-1], y = yTest,
-                                        predict_function = p_rf)
-
-
 explain_rf <- explain(classif_rf, label = "RF",
                       data = Test[,-1], y = yTest,
                       predict_function = p_rf)
@@ -144,9 +124,8 @@ explain_rf <- explain(classif_rf, label = "RF",
 
 
 mp_glm <- model_performance(explain_glm) %>% as.data.frame()
-mp_ann  <- model_performance(explain_ann) %>% as.data.frame()
 mp_rf <- model_performance(explain_rf) %>% as.data.frame()
-mp_all <- rbind(mp_glm,mp_ann,mp_rf)
+mp_all <- rbind(mp_glm,mp_rf)
 
 
 
@@ -167,10 +146,9 @@ dev.off()
 # Age
 
 ale_glm_age  <- variable_response(explain_glm , variable =  "AGE", type = "pdp")
-ale_ann_age  <- variable_response(explain_ann, variable =  "AGE", type = "pdp")
 ale_rf_age   <- variable_response(explain_rf, variable =  "AGE", type = "pdp")
 
-ale_age_all <- rbind(ale_glm_age ,ale_ann_age,ale_rf_age  )
+ale_age_all <- rbind(ale_glm_age,ale_rf_age)
 
 
 pdf("ale_age.pdf",height = 4,width = 5)
@@ -187,11 +165,6 @@ dev.off()
 ale_glm_TL1  <- variable_response(explain_glm , variable =  "TL_rand", type = "pdp")
 ale_glm_TL2   <- variable_response(explain_glm , variable =  "TLD_rand", type = "pdp")
 ale_glm_TL3  <- variable_response(explain_glm , variable =  "TLP_rand", type = "pdp")
-
-ale_ann_TL1   <- variable_response(explain_ann, variable =  "TL_rand", type = "pdp")
-ale_ann_TL2   <- variable_response(explain_ann, variable =  "TLD_rand", type = "pdp")
-ale_ann_TL3  <- variable_response(explain_ann, variable =  "TLP_rand", type = "pdp")
-
 
 ale_rf_TL1   <- variable_response(explain_rf, variable =  "TL_rand", type = "pdp")
 ale_rf_TL2   <- variable_response(explain_rf, variable =  "TLD_rand", type = "pdp")
@@ -218,12 +191,11 @@ dev.off()
 ## Ligation Group
 
 ale_glm_lg  <- variable_response(explain_glm , variable =  "LIGATION_GROUP", type = "factor")
-ale_ann_lg  <- variable_response(explain_ann, variable =  "LIGATION_GROUP", type = "factor")
 ale_rf_lg   <- variable_response(explain_rf, variable =  "LIGATION_GROUP", type = "factor")
 
 
 pdf("ale_LG.pdf",height = 7.5,width = 8.75)
-plot(ale_glm_lg,ale_svm_lg,ale_rf_lg) + my_style()
+plot(ale_glm_lg,ale_rf_lg) + my_style()
 dev.off()
 
 ## Anamastosis
@@ -251,8 +223,6 @@ lfu(as.numeric(as.character(Train$PREGNANT_NUMERIC)),p_rf(classif_rf))
 
 vi_glm <- variable_importance(explain_glm,  n_sample = -1,loss_function = lfu,type = "difference")
 
-vi_ann <- variable_importance(explain_ann,  n_sample = -1,loss_function = lfu,type = "difference") 
-
 vi_rf <- variable_importance(explain_rf,n_sample = -1,loss_function = lfu,type = "difference") 
 
 `%not_in%` <- purrr::negate(`%in%`)
@@ -279,131 +249,10 @@ ggplot(vi_all,aes(x=variable,y=dropout_loss,fill = label)) +
 
 
 
-
-
-
-
-
-
-
-ggplot(vi_classif_gbm, aes(x=variable, y=dropout_loss)) +
-  geom_segment( aes(x=variable, xend=variable, y=1, yend=dropout_loss), color="skyblue") +
-  geom_point( color="blue", size=4, alpha=0.6) +
-  theme_light() +
-  coord_flip() +
-  my_style() 
-
-
-pdp_classif_xgb  <- variable_response(explainer_classif_xgb, variable =  "TLP_rand", type = "ale")
-pdp_classif_gbm  <- variable_response(explainer_classif_gbm, variable =  "TLP_rand", type = "ale")
-pdp_classif_glm  <- variable_response(explainer_classif_glm, variable = "TLP_rand", type = "ale")
-plot(pdp_classif_xgb, pdp_classif_gbm, pdp_classif_glm ) +  my_style() 
-
-
-
-
-
 svd_rf  <- single_variable(explainer_classif_xgb , variable = "LIGATION_GROUP", type = "factor")
 
 plot(svd_rf) +  my_style() 
 
-
-
-
-
-
-model_matrix_train <- model.matrix(PREGNANT_NUMERIC~AGE+TL_rand + 
-                                     TLD_rand + TLP_rand  + 
-                                     ANAS_rand + Fibr_rand-1,Train)
-
-model_matrix_test <- model.matrix(PREGNANT_NUMERIC~AGE+TL_rand + 
-                                     TLD_rand + TLP_rand  + 
-                                     ANAS_rand + Fibr_rand-1,Test)
-
-data_train <- xgb.DMatrix(model_matrix_train,
-                          label = Train$PREGNANT_NUMERIC)
-
-data_test <- xgb.DMatrix(model_matrix_test,
-                         label = Test$PREGNANT_NUMERIC)
-
-
-param <- list(max_depth=2,objective="binary:logistic")
-
-HR_xgb_model <- xgb.train(param,data_train,nrounds=50)
-
-HR_glm_model <- glm(PREGNANT_NUMERIC~AGE+TL_rand + 
-                      TLD_rand + TLP_rand  + 
-                      ANAS_rand + Fibr_rand,Train,family=binomial(link = "logit"))
-
-
-
-
-
-explainer_glm <- explain(HR_glm_model,Train)
-
-expl_glm1 <- variable_response(explainer_glm,"AGE",
-                              "pdp")
-expl_glm2 <- variable_response(explainer_glm,"TL_rand",
-                               "pdp")
-expl_glm3 <- variable_response(explainer_glm,"TLD_rand",
-                               "pdp")
-expl_glm4 <- variable_response(explainer_glm,"TLP_rand",
-                               "pdp")
-expl_glm5 <- variable_response(explainer_glm,"ANAS_rand",
-                               "pdp")
-expl_glm6 <- variable_response(explainer_glm,"Fibr_rand",
-                               "pdp")
-
-expl_glm <- rbind(expl_glm1,expl_glm2,expl_glm3,expl_glm4,expl_glm5,
-                  expl_glm6)
-
-
-explainer_xgb <- explain(HR_xgb_model,model_matrix_train,
-                         y=Train$PREGNANT_NUMERIC,
-                         label="modelxgb")                        
-
-expl_xgb1 <- variable_response(explainer_xgb,"AGE",
-                              "pdp")
-expl_xgb2 <- variable_response(explainer_xgb,"TL_rand",
-                              "pdp")
-expl_xgb3 <- variable_response(explainer_xgb,"TLD_rand",
-                              "pdp")
-expl_xgb4 <- variable_response(explainer_xgb,"TLP_rand",
-                              "pdp")
-expl_xgb5 <- variable_response(explainer_xgb,"ANAS_rand",
-                              "pdp")
-expl_xgb6 <- variable_response(explainer_xgb,"Fibr_rand",
-                               "pdp")
-
-expl_xgb <- rbind(expl_xgb1,expl_xgb2,expl_xgb3,expl_xgb4,expl_xgb5,
-                  expl_xgb6)
-
-expl <- rbind(expl_glm,expl_xgb) %>%
-mutate(var = recode(var, TL_rand = "Tube length",
-                    TLD_rand = "Tube length distal",
-                    TLP_rand = "Tube length prox",
-                    ANAS_rand = "ANASTOMOSIS",
-                    Fibr_rand = "FIBROSIS")) %>%
-  mutate(label = recode(label, lm = "glm",
-                        xgb.Booster = "xgb"))
-
-
-pdf("partial_dep.pdf",height = 7,width = 10)
-ggplot(expl,aes(x=x,y=y,group=label,color=label)) +
-  geom_line() + geom_point() + scale_colour_tableau()+
-  facet_wrap(.~var,scales="free") + my_style() +
-  ylab("Pregnancy probability") + xlab("")
-dev.off()
-
-
-
-vd_xgb <- variable_importance(explainer_xgb,type="difference")
-
-plot(vd_xgb)
-
-nobs <- model_matrix_train[1, , drop = FALSE]
-pred_xgb <- prediction_breakdown(explainer_xgb,observation = nobs)
-plot(pred_xgb )
 
 
 
